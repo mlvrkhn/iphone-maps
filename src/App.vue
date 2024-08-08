6a2b9acc1bca55ff5b915a8e3456d929ec0cffb4
mlvrkhn <martin.guide@gmail.com>
2024-08-09 00:09:43 +0200

    suggestions search

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       :search="getCoordinates"
       placeholder="Enter city name"
       aria-label="Enter city name"
+      :get-result-value="getResultValue"
     ></autocomplete>
     <div v-if="coordinates">
       <h2>Coordinates for {{ city }}:</h2>
@@ -27,26 +28,31 @@ const coordinates = ref(null);
 const error = ref(null);
 
 const getCoordinates = async (input) => {
-  console.log("🚀   getCoordinates  input:", input);
   try {
-    if (input.length < 1) {
-      return [];
-    }
-    const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${input},+po&language=en&session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
-    const response = await fetch(url);
-    const data = await response.json().suggestions[0];
-    console.log("🚀   getCoordinates  data:", data);
-    coordinates.value = {
-      lat: data.coord.lat,
-      lng: data.coord.lon,
-    };
+    const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${encodeURI(
+      input
+    )},+po&language=en&session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
+    // const response = await fetch(url);
+
+    return new Promise((resolve) => {
+      if (input.length < 3) {
+        return resolve([]);
+      }
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          resolve(data.suggestions);
+        });
+    });
   } catch (err) {
+    console.log("🚀   getCoordinates  err:", err);
     error.value = "City not found";
   }
 };
 
 const getResultValue = (result) => {
-  console.log(result);
+  return result.suggestions;
 };
 </script>
 
