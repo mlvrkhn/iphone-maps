1bb0bae1dd841e0e56b7f73e8d2bb045a293f4af
mlvrkhn <martin.guide@gmail.com>
2024-08-09 16:37:14 +0200

    coordinates

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container" id="app">
     <autocomplete
-      :search="getCoordinates"
+      :search="search"
       placeholder="Enter city name"
       aria-label="Enter city name"
       :get-result-value="getResultValue"
+      @submit="onSubmit"
     ></autocomplete>
     <div v-if="coordinates">
       <h2>Coordinates for {{ city }}:</h2>
@@ -27,7 +28,7 @@ const city = ref("");
 const coordinates = ref(null);
 const error = ref(null);
 
-const getCoordinates = async (input) => {
+const search = async (input) => {
   try {
     const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${encodeURI(
       input
@@ -50,6 +51,32 @@ const getCoordinates = async (input) => {
 
 const getResultValue = (result) => {
   return result.name;
+};
+
+const onSubmit = (result) => {
+  // console.log(result);
+  try {
+    const url = `https://api.mapbox.com/search/searchbox/v1/retrieve/dXJuOm1ieHBvaTo3ZWIyMWE0ZS1jZGYzLTQxNGUtYjJhNy02ZDFmOWE3YzdiNDA?session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
+    return new Promise((resolve) => {
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          coordinates.value = {
+            lat: data.features[0].geometry.coordinates[1],
+            lng: data.features[0].geometry.coordinates[0],
+          };
+          // resolve(data.features[0])?.properties.coordinates;
+        });
+      console.log(coordinates.value);
+    });
+  } catch (err) {
+    error.value = "Error error";
+  }
+
+  // coordinates.value = {
+  //   lat: result.latlng.lat,
+  //   lng: result.latlng.lng,
+  // };
 };
 </script>
 
