c669b2fac048c11e29c96b195483dbd7c497851d
mlvrkhn <martin.guide@gmail.com>
2024-08-09 16:50:27 +0200

    correct coords

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,93 @@
 <template>
-  <div class="container" id="app">
-    <autocomplete
-      :search="search"
-      placeholder="Enter city name"
-      aria-label="Enter city name"
-      :get-result-value="getResultValue"
-      @submit="onSubmit"
-    ></autocomplete>
-    <div v-if="coordinates">
-      <h2>Coordinates for {{ city }}:</h2>
-      <p>Latitude: {{ coordinates.lat }}</p>
-      <p>Longitude: {{ coordinates.lng }}</p>
-    </div>
+	<div class="container" id="app">
+		<autocomplete
+			:search="search"
+			placeholder="Enter city name"
+			aria-label="Enter city name"
+			:get-result-value="getResultValue"
+			@submit="onSubmit"
+		></autocomplete>
+		<div v-if="coordinates">
+			<h2>Coordinates for {{ city }}:</h2>
+			<p>Latitude: {{ coordinates.lat }}</p>
+			<p>Longitude: {{ coordinates.lng }}</p>
+		</div>
 
-    <div v-if="error">
-      <p style="color: red">{{ error }}</p>
-    </div>
-    <button @click="downloadMap">Download</button>
-  </div>
+		<div v-if="error">
+			<p style="color: red">{{ error }}</p>
+		</div>
+		<button @click="downloadMap">Download</button>
+	</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import Autocomplete from "@trevoreyre/autocomplete-vue";
+import { ref } from 'vue';
+import Autocomplete from '@trevoreyre/autocomplete-vue';
 
-const city = ref("");
+const city = ref('');
 const coordinates = ref(null);
+const mapId = ref('');
 const error = ref(null);
 
-const search = async (input) => {
-  try {
-    const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${encodeURI(
-      input
-    )},+po&language=en&session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
-    return new Promise((resolve) => {
-      if (input.length < 3) {
-        return resolve([]);
-      }
+const search = async input => {
+	try {
+		const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${encodeURI(
+			input
+		)},+po&language=en&session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
+		return new Promise(resolve => {
+			if (input.length < 3) {
+				return resolve([]);
+			}
 
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          resolve(data.suggestions);
-        });
-    });
-  } catch (err) {
-    error.value = "City not found";
-  }
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					mapId.value = data.suggestions[0].id;
+					resolve(data.suggestions);
+				});
+		});
+	} catch (err) {
+		error.value = 'City not found';
+	}
 };
 
-const getResultValue = (result) => {
-  return result.name;
+const getResultValue = result => {
+	return result.name;
 };
 
-const onSubmit = (result) => {
-  // console.log(result);
-  try {
-    const url = `https://api.mapbox.com/search/searchbox/v1/retrieve/dXJuOm1ieHBvaTo3ZWIyMWE0ZS1jZGYzLTQxNGUtYjJhNy02ZDFmOWE3YzdiNDA?session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
-    return new Promise((resolve) => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          coordinates.value = {
-            lat: data.features[0].geometry.coordinates[1],
-            lng: data.features[0].geometry.coordinates[0],
-          };
-          // resolve(data.features[0])?.properties.coordinates;
-        });
-      console.log(coordinates.value);
-    });
-  } catch (err) {
-    error.value = "Error error";
-  }
+const onSubmit = result => {
+	console.log('er: ', result);
+	try {
+		const url = `https://api.mapbox.com/search/searchbox/v1/retrieve/${result.mapbox_id}?session_token=0f206044-3a78-42e1-8913-383ccd20b30b&access_token=pk.eyJ1IjoibWx2cmtobiIsImEiOiJjbHpsbzkzanMwM2kyMnJwbmo1dnhkZTlkIn0.eYXg3LX3YBuBLcY0Q6gMIQ`;
+		return new Promise(resolve => {
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					coordinates.value = {
+						lat: data.features[0].properties.coordinates.latitute,
+						lng: data.features[0].properties.coordinates.longitude,
+					};
+					resolve(data);
+					// resolve(data.features[0])?.properties.coordinates;
+				});
+			console.log(coordinates.value);
+		});
+	} catch (err) {
+		error.value = 'Error error';
+	}
 
-  // coordinates.value = {
-  //   lat: result.latlng.lat,
-  //   lng: result.latlng.lng,
-  // };
+	// coordinates.value = {
+	//   lat: result.latlng.lat,
+	//   lng: result.latlng.lng,
+	// };
 };
 </script>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: row;
-  /* align-items: center;
+	display: flex;
+	flex-direction: row;
+	/* align-items: center;
   justify-content: center; */
 }
 </style>
